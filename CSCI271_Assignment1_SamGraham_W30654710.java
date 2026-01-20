de956a0c3110b6b55f87ef03fa9d96cdacfd681d
SamGraham1 <156004991+SamGraham1@users.noreply.github.com>
2026-01-20 12:10:38 -0500

    added header

---
+++ CSCI271_Assignment1_SamGraham_W30654710.java
@@ -0,0 +1,13 @@
+/*************************************************************************
+* Project 1 for CSCI 271-001 Spring 2026
+*
+* Author: Sam Graham
+* OS: Ubuntu Debian Linux 21.1
+* Compiler: javac 25.0.1
+* Date: January 23, 2026
+*
+* Purpose
+* This program reads a list of scores from an input file, counts how many
+* scores there are in the list, and computes their sum and average.
+*
+*************************************************************************/
