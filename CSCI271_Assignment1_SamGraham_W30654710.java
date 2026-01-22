fb3c767cdc4286ce6c7e6433d308e656e3774a23
SamGraham1 <156004991+SamGraham1@users.noreply.github.com>
2026-01-22 10:53:31 -0500

    Initial formatting comments and class

--- CSCI271_Assignment1_SamGraham_W30654710.java
+++ CSCI271_Assignment1_SamGraham_W30654710.java
@@ -1,13 +1,55 @@
+/*******************************************************************
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+*   instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+*   or any unauthorized sources, including the Internet, either
+*   modified or unmodified.
+* - If any source code or documentation used in my program was
+*   obtained from other sources, like a textbook or course notes,
+*   I have clearly indicated that with a proper citation in the
+*   comments of my program.
+* - I have not designed this program in such a way as to defeat or
+*   interfere with the normal operation of the supplied grading code.
+*
+* Sam Graham
+* W300654719
+********************************************************************/
+
 /*************************************************************************
-* Project 1 for CSCI 271-001 Spring 2026
-*
-* Author: Sam Graham
-* OS: Ubuntu Debian Linux 21.1
-* Compiler: javac 25.0.1
-* Date: January 23, 2026
-*
-* Purpose
-* This program reads a list of scores from an input file, counts how many
-* scores there are in the list, and computes their sum and average.
-*
-*************************************************************************/
+ * Project 1 for CSCI 271-001 Spring 2026
+ *
+ * Author: Sam Graham
+ * OS: Ubuntu Linux 21.10
+ * Compiler: javac 25.0.1
+ * Date: January 23, 2026
+ *
+ * Purpose:
+ * This program calculates the final numeric grade for a student based
+ * on assignment scores, test scores, midterm, and final exam according
+ * to the grading scheme in the course syllabus. The calculation includes
+ * a corrected handling of assignment weights for the 60-80 E range.
+ *
+ *************************************************************************/
+
+import java.util.*;
+
+public class CSCI271_Assignment1_SamGraham_W300654719
+{
+    /*****************************main****************************
+    * Description: Prompts the user to enter scores for assignments,
+    *              tests, midterm, and final exam, then calculates
+    *              the final numeric grade according to the syllabus.
+    *
+    * Parameters: args (input): command line arguments (not used)
+    *
+    * Pre: All scores entered must be numeric and between 0 and 100
+    *
+    * Post: Final grade (G) is calculated and displayed to the user
+    *
+    * Returns: none
+    *
+    * Called by: none
+    * Calls: none
+    ************************************************************************/
+}
